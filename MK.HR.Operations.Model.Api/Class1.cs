80608fe915a4d56e7e5be8dbbbd92bfbd0871b3d
Manjunath Krishnappa <>
2020-01-08 15:33:39 +0530

    Initial Project Structure

---
+++ MK.HR.Operations.Model.Api/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MK.HR.Operations.Model.Api
+{
+	public class Class1
+	{
+	}
+}
---
+++ MK.HR.Operations.Model/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MK.HR.Operations.Model
+{
+	public class Class1
+	{
+	}
+}

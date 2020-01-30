88cc7ba1d156e503e63b048201b2cc27610f0df8
Manjunath Krishnappa <manju_25gowda@yahoo.com>
2020-01-30 14:52:05 +0530

    Added application layout using flex layout module

--- MK-HR-Operations-App/src/app/hrops/hrops-routing.module.ts
+++ MK-HR-Operations-App/src/app/hrops/hrops-routing.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { EmployeeComponent } from './employee/employee.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
 
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'employee', component: EmployeeComponent },
-  { path: '**', redirectTo: 'home' }
+  { path: '', component: HomeComponent,
+    children: [
+      { path: '', component: MainContentComponent },
+      { path: 'employee', component: EmployeeComponent },
+      { path: 'employee', component: EmployeeComponent },
+      { path: 'employee', component: EmployeeComponent }
+    ]
+  },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
--- MK-HR-Operations-App/src/app/hrops/hrops.module.ts
+++ MK-HR-Operations-App/src/app/hrops/hrops.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
 
 import { HropsRoutingModule } from './hrops-routing.module';
 import { MaterialModule } from '../shared/material.module';
-import { FormsModule } from '@angular/forms';
 
 import { HomeComponent } from './home/home.component';
 import { EmployeeComponent } from './employee/employee.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, EmployeeComponent],
+  declarations: [HomeComponent, EmployeeComponent, ToolbarComponent, MainContentComponent, SidenavComponent],
   imports: [
     CommonModule,
-    HropsRoutingModule,
     MaterialModule,
-    FormsModule
+    FlexLayoutModule,
+    FormsModule,
+    HropsRoutingModule
   ]
 })
 export class HropsModule { }

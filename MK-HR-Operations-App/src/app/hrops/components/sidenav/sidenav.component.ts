c7d983601961679de91c1e3a7ac3c928869b6018
Manjunath Krishnappa <manju_25gowda@yahoo.com>
2020-01-30 16:14:08 +0530

    Layout setup completed and applied responsiveness

--- MK-HR-Operations-App/src/app/hrops/components/sidenav/sidenav.component.ts
+++ MK-HR-Operations-App/src/app/hrops/components/sidenav/sidenav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+const WIDTH_OF_SMALL_SCREEN = 720;
+
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavComponent implements OnInit {
 
+  private mediaMatcher: MediaQueryList = matchMedia(`(max-width: ${WIDTH_OF_SMALL_SCREEN}px)`);
   constructor() { }
 
   ngOnInit() {
+  }
+  showFiller = false;
+
+  isSmallScreen(): boolean {
+    // 'True' will be retured if the screen is found to be less than 720px
+    return this.mediaMatcher.matches;
   }
 
 }
